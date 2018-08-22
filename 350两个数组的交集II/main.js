fc9a94945543126719ac25862dd81faca7c0a869
think2cat <gavin2026@gmail.com>
2018-08-22 16:06:41 +0800

    🔆重做 350两个数组的交集II

--- 350两个数组的交集II/main.js
+++ 350两个数组的交集II/main.js
@@ -5,36 +5,13 @@
  */
 var intersect = function(nums1, nums2) {
     console.log('begin',nums1,nums2)
-	// 测试用例有问题，已发邮件询问
-	// 提示输入 [3,1,2] [1,1]
-	// 实际传入 [ 2, 1 ] [ 1, 2 ]
-	let check = (a1, a2) => {
-		console.log('check',a1,a2)
-		let tmp = []
-		a1.some((e, i) => {
-			// console.log(e, a2[i])
-			if(i < a2.length){
-				e === a2[i] && tmp.push(e)
-				return e !== a2[i]
-			} else {
-				return true
-			}
-		})
-		return tmp
-	}
 	let ret = []
-    for (let i = 0; i < nums1.length; i++) {
-		for (let j = 0; j < nums2.length; j++) {
+    for (let i = nums1.length - 1; i >= 0 ; i--) {
+		for (let j = nums2.length - 1; j >= 0; j--) {
 			if (nums1[i] == nums2[j]) {
-                // console.log('i',i,nums1.slice(i))
-                // console.log('j',j,nums2.slice(j))
-				let k = check(nums1.slice(i), nums2.slice(j))
-				if (k.length >= ret.length){
-                    // console.log("newMax", k)
-					ret = Object.assign([], k)
-				}
-				// j += k.length
-				// i += k.length
+                console.log(i, j, nums1[i])
+				ret.push(nums1.splice(i, 1)[0])
+				nums2.splice(j, 1)
 			}
 		}
 	}
