9842fd9e6b847c4793a9b165931d1312d31ee818
think2cat <gavin2026@gmail.com>
2018-08-22 16:19:20 +0800

    ⌨️优化 349两个数组的交集

--- 349两个数组的交集/main.js
+++ 349两个数组的交集/main.js
@@ -4,17 +4,20 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    console.log('begin',nums1,nums2)
+    // console.log('begin',nums1,nums2)
+	nums1 = [...new Set(nums1)]
+	nums2 = [...new Set(nums2)]
 	let ret = []
     for (let i = nums1.length - 1; i >= 0 ; i--) {
 		for (let j = nums2.length - 1; j >= 0; j--) {
-			if (nums1[i] == nums2[j]) {
-                console.log(i, j, nums1[i])
-				ret.push(nums1.splice(i, 1)[0])
+			if (nums1[i] === nums2[j]) {
+                // console.log(i, j, nums1[i])
+				ret.push(nums1[i])
+                nums1.splice(i, 1)
 				nums2.splice(j, 1)
 			}
 		}
 	}
-	console.log("return", ret)
-	return [...new Set(ret)]
+	// console.log("return", ret)
+	return ret
 };
